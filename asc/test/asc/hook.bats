af172c3f6605a86668280264eb3f3771420dd570
Paulmicha <paul.michalet@gmail.com>
2017-12-31 10:17:56 -0200

    [wip] Failed tests backup.

--- asc/test/asc/hook.bats
+++ asc/test/asc/hook.bats
@@ -1,6 +1,15 @@
 #!/usr/bin/env bats
 
+setup() {
+  . asc/bootstrap.sh
+}
+
 @test "1. Single action hook" {
-  result="$(bash asc/custom/debug.sh)"
-  [ -n "$result" ]
+  # result="$(hook -a 'bootstrap')"
+  # [ -n "$result" ]
+  run hook -a 'bootstrap'
+  my_var="$ASC_INC"
+  # result="test"
+  # [ -n "$result" ]
+  [ -n "$my_var" ]
 }
--- asc/test/run.sh
+++ asc/test/run.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env bash
 
 ##
-# [wip] Run tests.
+# TODO [wip] Run tests.
 #
-# Usage :
-# $ asc/test/run.sh
+# TODO evaluate plan : forward all arguments of this script to a hook call
+# with predefined 'test' subject (that could be prefixed or suffixed with custom
+# values).
+#
+# E.g. :
+# $ hook -s 'test' -a "$1"
+#
+# @example (planned)
+#   . asc/bootstrap.sh
+#   . asc/test/run.sh 'asc'
+#
+# @example (current WIP)
+#   asc/test/run.sh
 #
 
-# [wip] debug.
-for file in $(find asc/test/asc -maxdepth 1 -type f -print0 | xargs -0); do
-  bats "$file"
+. asc/bootstrap.sh
+
+# TODO prereq BATS installed - to provision (or bundle in a 'vendors' dir ?)
+tf_path=asc/test/asc
+bats_files=$(u_fs_file_list $tf_path 1 '*.bats')
+
+for file in $bats_files; do
+  bats "$tf_path/$file"
 done
