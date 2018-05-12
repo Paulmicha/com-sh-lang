6fb71e2c8db6b5a9abd8d16e90afcec802e3312c
Paulmicha <paul.michalet@gmail.com>
2018-05-12 15:54:25 -0300

    [wip] Add reg del actions (host + instance).

---
+++ asc/host/registry_del.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+##
+# [abstract] Deletes host-level registry value.
+#
+# Removes given entry from an abstract host-level storage by given key.
+# "Abstract" means that ASC core itself doesn't provide any actual
+# implementation for this functionality. It is necessary to use an extension
+# which does. E.g. :
+# @see asc/extensions/file_registry
+#
+# @example
+#   asc/host/registry_del.sh my_key
+#
+
+. asc/bootstrap.sh
+u_host_registry_del $@
---
+++ asc/instance/registry_del.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+##
+# [abstract] Deletes instance-level registry value.
+#
+# Removes given entry from an abstract instance-level storage by given key.
+# "Abstract" means that ASC core itself doesn't provide any actual
+# implementation for this functionality. It is necessary to use an extension
+# which does. E.g. :
+# @see asc/extensions/file_registry
+#
+# @example
+#   asc/instance/registry_del.sh my_key
+#
+
+. asc/bootstrap.sh
+u_instance_registry_del $@
