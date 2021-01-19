b6422d68185d36f3b674ffc8e539f7da9d5977b2
Paulmicha <paul.michalet@gmail.com>
2021-01-19 14:51:39 -0300

    Move basic auth logic to u_str_basic_auth_credentials() + update drupalwt_d4d extesion accordingly + [wip] Debug deferred globals assigment when using Yaml overrides.

--- asc/instance/reinit.sh
+++ asc/instance/reinit.sh
@@ -3,12 +3,15 @@
 ##
 # Reinitializes current project instance without changing existing settings.
 #
-# Rewrites locally generated ASC files based on the values initially set for the
-# following global env. vars :
+# Rewrites locally generated ASC files while keeping the values - if previously
+# set - for the following global env. vars :
 # - $INSTANCE_TYPE
 # - $INSTANCE_DOMAIN
+# - $DC_NS
 # - $HOST_TYPE
 # - $PROVISION_USING
+# - $ASC_SSH_PUBKEY
+# - $ASC_DB_ID
 #
 # @example
 #   make reinit
@@ -16,14 +19,13 @@
 #   asc/instance/reinit.sh
 #
 
-# The only values we're using when re-initializing are ASC 'core' globals.
+# Can't have read-only variables here, so we need to extract just the
+# variables we need.
+# TODO support all globals for reinits ? For ex. as in :
+# @see u_traefik_generate_acme_conf() in asc/extensions/remote_traefik/remote_traefik.inc.sh
+# -> here, we could just pass a custom option that would instruct the
+# u_instance_init() function to dynamically get all existing values ?
 if [[ -f '.env' ]]; then
-  # Can't have read-only variables here, so we need to extract just the
-  # variables we need.
-  # TODO support all globals for reinits ? For ex. as in :
-  # @see u_traefik_generate_acme_conf() in asc/extensions/remote_traefik/remote_traefik.inc.sh
-  # -> here, we could just pass a custom option that would instruct the
-  # u_instance_init() function to dynamically get all existing values ?
   while IFS= read -r line _; do
     case "$line" in
       'INSTANCE_TYPE='*)
@@ -41,23 +43,32 @@ if [[ -f '.env' ]]; then
       'PROVISION_USING='*)
         eval "$line"
         ;;
+      'ASC_SSH_PUBKEY='*)
+        eval "$line"
+        ;;
+      'ASC_DB_ID='*)
+        eval "$line"
+        ;;
     esac
   done < '.env'
 fi
 
+# TODO [wip] rework globals to take Yaml files into consideration during
+# deferred values assignement. Simplifies reinit (remove "env -i") :
+
 # Wipe out env vars to avoid pile-ups for 'append' type globals during reinit.
 # See https://unix.stackexchange.com/a/49057
-env -i \
-  # Except individual public key path for ASC remote instances operations.
-  # @see scripts/asc/extend/remote/post_init.hook.sh
-  ASC_SSH_PUBKEY="$ASC_SSH_PUBKEY" \
-  # Also except ASC_DB_ID for the db extension.
-  # @see u_db_set() in asc/extensions/db/db.inc.sh
-  ASC_DB_ID="$ASC_DB_ID" \
-  # Also except common shell env vars some programs use.
-  HOME="$HOME" LC_CTYPE="${LC_ALL:-${LC_CTYPE:-$LANG}}" PATH="$PATH" USER="$USER" \
-  # Now we're good.
-  . asc/instance/init.sh \
+# Except individual public key path for ASC remote instances operations.
+# @see scripts/asc/extend/remote/post_init.hook.sh
+# Also except ASC_DB_ID for the db extension.
+# @see u_db_set() in asc/extensions/db/db.inc.sh
+# Also except common shell env vars some programs use.
+# env -i \
+#   ASC_SSH_PUBKEY="$ASC_SSH_PUBKEY" \
+#   ASC_DB_ID="$ASC_DB_ID" \
+#   HOME="$HOME" LC_CTYPE="${LC_ALL:-${LC_CTYPE:-$LANG}}" PATH="$PATH" USER="$USER" \
+
+. asc/instance/init.sh \
     -t "$INSTANCE_TYPE" \
     -d "$INSTANCE_DOMAIN" \
     -c "$DC_NS" \
