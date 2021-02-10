b36271ab0306dac3c3af81969345e927f2ef0158
Paulmicha <paul.michalet@gmail.com>
2021-02-10 00:08:37 -0300

    Improve 'db' extension for multi-db projects dealing with different DB drivers.

--- asc/extensions/pgsql/instance/wait_for.docker-compose.hook.sh
+++ asc/extensions/pgsql/instance/wait_for.docker-compose.hook.sh
@@ -5,32 +5,32 @@
 #
 # Wait until the database container accepts connections. Examples :
 # See https://github.com/wodby/docker4drupal/blob/master/tests/8/run.sh
-# See https://github.com/wodby/mariadb/blob/master/10/bin/actions.mk
+# See https://github.com/wodby/postgres/blob/master/bin/wait_for_postgres
 # See https://github.com/wodby/alpine/blob/master/bin/wait_for
 # @see asc/utilities/shell.sh
 # @see asc/extensions/docker-compose/instance/start.docker-compose.hook.sh
 # @see asc/extensions/docker-compose/instance/instance.inc.sh
 # @see asc/instance/start.sh
 #
-# Uses bash aliases defined for mariadb (mysql).
-# @see asc/extensions/mysql/asc/alias.docker-compose.hook.sh
+# Uses bash aliases defined for postgres.
+# @see asc/extensions/pgsql/asc/alias.docker-compose.hook.sh
 #
 
 # All databases information is already loaded at this point.
 # @see asc/extensions/db/instance/pre_start.hook.sh
-# So all we have to do here is to check that every *MySQL* database is ready.
+# So all we have to do here is to check that every PgSQL database is ready.
 db_ids=()
 u_db_get_ids
 
 for db_id in "${db_ids[@]}"; do
   u_str_uppercase "${db_id}_DB_DRIVER"
 
-  case "${!uppercase}" in 'mysql')
+  case "${!uppercase}" in 'pgsql')
     # We still have to call u_db_set() again for aliases targeting specific
     # docker-compose services.
     u_db_set "$db_id"
 
-    wait_for "MySQL '$db_id' db" \
-      "mysqladmin --user='$DB_ADMIN_USER' --password='$DB_ADMIN_PASS' --host='$DB_HOST' --port=$DB_PORT status &> /dev/null"
+    wait_for "PgSQL '$db_id' db" \
+      "pg_isready -h$DB_HOST -p$DB_PORT -U$DB_USER -d$DB_NAME -t1 &> /dev/null"
   esac
 done
